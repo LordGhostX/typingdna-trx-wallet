926b6fa054d466848b10e6940e9f03df91814552
LordGhostX <47832826+LordGhostX@users.noreply.github.com>
2021-02-23 17:13:07 +0100

    Update typingdna.py

--- with-typingdna/typingdna.py
+++ with-typingdna/typingdna.py
@@ -22,25 +22,6 @@ class TypingDNA:
             "custom_field": custom_field
         }
         return requests.post(url, headers=self.headers, data=data)
-
-    def save(self, id, tp, custom_field=None):
-        url = f"{self.base_url}/save/{id}"
-        data = {
-            "tp": tp,
-            "custom_field": custom_field
-        }
-        return requests.post(url, headers=self.headers, data=data)
-
-    def verify(self, id, tp, quality, device_similarity_only=None, position_only=None, custom_field=None):
-        url = f"{self.base_url}/verify/{id}"
-        data = {
-            "tp": tp,
-            "quality": quality,
-            "device_similarity_only": device_similarity_only,
-            "position_only": position_only,
-            "custom_field": custom_field
-        }
-        return requests.post(url, headers=self.headers, data=data)
-
+    
     def hash_text(self, text):
         return hashlib.sha1((text + text[::-1]).encode()).hexdigest()
